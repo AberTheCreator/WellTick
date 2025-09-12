ed28afaae380ac910299ea7adf8288ca73a58892
AberTheCreator <PaulAber68@gmail.com>
2025-09-12 09:38:50 +0100

    Create Emergency.tsx

---
+++ frontend/src/pages/Emergency.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import EmergencyContacts from '../components/Emergency/EmergencyContacts';
+
+const Emergency: React.FC = () => {
+  return <EmergencyContacts />;
+};
+
+export default Emergency;
