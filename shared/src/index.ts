6ed52a30d1f7b8330ce99e026ec52ede4d50c6e3
AberTheCreator <PaulAber68@gmail.com>
2025-09-13 20:56:26 +0100

    Create index.ts

---
+++ shared/src/index.ts
@@ -0,0 +1,15 @@
+export * from './types';
+
+export type {
+  User,
+  UserPreferences,
+  CommunityPost,
+  Place,
+  EmergencyContact,
+  HealthRecord,
+  WellnessMetric,
+  AIMessage,
+  Web3Config,
+  AccessibilityCredential,
+  TokenReward
+} from './types';
